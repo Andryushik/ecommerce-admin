7913c9d11f8e8c42666961e6899460600f602714
Andrei <andryushik@gmail.com>
2023-08-09 13:04:27 +0200

    modal provider is done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,21 @@
 'use client';
 
+import { useEffect } from 'react';
+
 import { Modal } from '@/components/ui/modal';
+import { useStoreModal } from '@/hooks/use-store-modal';
 
 const SetupPage = () => {
-  return (
-    <div className="p-4">
-      <Modal title="Test" description="Test Desc" isOpen onClose={() => {}}>
-        Children
-      </Modal>
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root page</div>;
 };
 
 export default SetupPage;

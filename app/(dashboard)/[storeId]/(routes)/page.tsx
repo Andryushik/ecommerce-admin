ef921b9728b860e9e63dd74297c8ffe2b4a22c4f
Andrei <andryushik@gmail.com>
2023-08-13 17:55:24 +0200

    navbar done

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,18 @@
-const DashboardPage = () => {
-  return <div>This is Dashboard</div>;
+import prismadb from '@/lib/prismadb';
+
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: { id: params.storeId },
+  });
+  return (
+    <div>
+      This is Dashboard <br /> Active store: {store?.name}
+    </div>
+  );
 };
 
 export default DashboardPage;

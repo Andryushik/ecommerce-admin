9a93a638e902812f98334cf17842cd1cfafa5d8b
Andrei <andryushik@gmail.com>
2023-08-19 15:07:09 +0200

    billboard routes done v3.47

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BillboardPage = async ({
+  params,
+}: {
+  params: { billboardId: string };
+}) => {
+  return <div>This is a form for billboards</div>;
+};
+
+export default BillboardPage;

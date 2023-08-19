019b7824e8c34a82d39c5c04821d3276a1be474a
Andrei <andryushik@gmail.com>
2023-08-19 18:47:07 +0200

    store to billboard but still errors

--- app/api/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -12,7 +12,7 @@ export async function GET(
       return new NextResponse('Billboard id is required', { status: 400 });
     }
 
-    const billboard = await prismadb.store.findUnique({
+    const billboard = await prismadb.billboard.findUnique({
       where: { id: params.billboardId },
     });
 
@@ -49,7 +49,7 @@ export async function PATCH(
       return new NextResponse('Billboard id is required', { status: 400 });
     }
 
-    const storeByUserId = await prismadb.store.findFirst({
+    const storeByUserId = await prismadb.billboard.findFirst({
       where: {
         id: params.storeId,
         userId,
@@ -98,7 +98,7 @@ export async function DELETE(
       return new NextResponse('Unauthorized', { status: 403 });
     }
 
-    const billboard = await prismadb.store.deleteMany({
+    const billboard = await prismadb.billboard.deleteMany({
       where: { id: params.billboardId },
     });
 
